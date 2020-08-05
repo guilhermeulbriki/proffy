42470bfbe61083a231387e16fad2857176d9997c
guilhermeulbriki <guilhermeulbriki@gmail.com>
2020-08-05 11:24:11 -0300

    day 3

--- packages/web/src/components/TeacherItem/index.tsx
+++ packages/web/src/components/TeacherItem/index.tsx
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface TeacherItemProps {
+  teacher: {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+  };
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const handleCreateNewConnection = useCallback(() => {
+    api.post('/connections', {
+      user_id: teacher.id,
+    });
+  }, [teacher.id]);
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/45516090?s=460&u=67689d03212e1da0974abc6eb687af42cedc2650&v=4"
-          alt="Guilherme Luís"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Guilherme Luís</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Tempor officia ipsum est reprehenderit ex enim. Aliquip fugiat
-        <br /> <br />
-        commodo voluptate sit amet est cillum et aliqua culpa nisi et qui
-        veniam. Sint tempor duis dolore nostrud do ipsum enim et labore duis
-        reprehenderit commodo. Mollit officia amet in aliqua reprehenderit.
-        Excepteur officia magna ipsum fugiat mollit ex nulla dolore. Excepteur
-        duis exercitation excepteur eiusmod anim. Ipsum veniam officia irure sit
-        dolor nostrud dolor eiusmod in dolore sint consequat.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
 
-        <button type="button">
+        <a
+          target="blank"
+          onClick={handleCreateNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
